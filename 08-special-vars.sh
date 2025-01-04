43df7bee70c7df76d567a5a59e4cb3a692fcb39f
durgaprasad0809 <durgaprasad.a0809@gmail.com>
2025-01-04 16:43:26 +0530

    updated the special vars code example

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -12,9 +12,11 @@ echo "Home directory of current user: $HOME"
 # To display the whos is user is running the scriptpresent working directory
 echo "Which user is running this script: $USER"
 
+# To display the all variables/allinput parameters in case if we given
 echo "All variables passed: $@"
 echo "Number of variables: $#"
 
+#To dsiplay the current and after 60 seconds of the script id
 echo "Process id of current script: $$"
 sleep 60 &
 echo "Process id of last command in background: $!"
