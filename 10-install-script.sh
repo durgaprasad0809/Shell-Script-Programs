79ffd9e779e71af553f741f9c7e8c244b403b9f9
durgaprasad0809 <durgaprasad.a0809@gmail.com>
2025-01-04 17:29:36 +0530

    updated the install the script shell code

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -13,12 +13,31 @@ fi
 # To Installation command of the Git Software
 dnf list installed git
 
+# To Installation the Git software without checking condtion whether this Git software is installed or not
+
+# if [ $? -ne 0 ]
+# then
+#     echo "Installing Git ... FAILURE"
+#     exit 1
+# else
+#     echo "Installing Git ... SUCCESS"
+# fi
+
+#To Installation the Git software with checking condtion whether this Git software is installed or not. 
+# In case if it is this Git software is already installed, it will show the already this Git is installed
+
 if [ $? -ne 0 ]
 then
-    echo "Installing Git ... FAILURE"
-    exit 1
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then
+        echo "Installing Git ... FAILURE"
+        exit 1
+    else
+        echo "Installing Git ... SUCCESS"
+    fi
 else
-    echo "Installing Git ... SUCCESS"
+    echo "Git is already ... INSTALLED"
 fi
 
 # To Installation command of the mysql Software
